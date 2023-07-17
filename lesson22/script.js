a14c436b1a913e0051c5b8e44b5b8000effbfd5b
Maksimilian13 <loskutov.maksxim@gmail.com>
2023-07-17 22:55:33 +0200

    finish

--- lesson22/script.js
+++ lesson22/script.js
@@ -1,50 +1,51 @@
 let numberFirst = prompt('number1');
 let numberSecond = prompt('number2');
 
-//let number1 = Number(numberFirst);
-//let number2 = Number(numberSecond);
-let comparison = +numberFirst < +numberSecond
-
+let comparison2 = +numberFirst > +numberSecond;
+let comparison1 = +numberFirst < +numberSecond;
 let additionResult = +numberFirst + +numberSecond;
-if(numberFirst === '' || numberSecond === '') {
-    alert('error');
-}
-else if(comparison) {
-        alert('Are you sure you want to continue');
-        alert(' AdditionResult ' + additionResult);
-    } 
-    else alert(' AdditionResult ' + additionResult); 
- 
 let subtractionResult = numberFirst - numberSecond;
-if(numberFirst === '' || numberSecond === '') {
-    alert('error');
-}
-else if(comparison) {
-        alert('Are you sure you want to continue');
-        alert(' Subtraction ' + subtractionResult);
-    }
-    else alert(' Subtraction ' + subtractionResult);
-
 let multiplicationResult = numberFirst * numberSecond;
-if(numberFirst === '' || numberSecond === '') {
-    alert('error');
-}
-else if(comparison) {
-        alert('Are you sure you want to continue');
-        alert(' Multiplication ' + multiplicationResult);
-    }     
-    else alert(' Multiplication ' + multiplicationResult);
-
 let divisionResult = numberFirst / numberSecond;
+
 if(numberFirst === '' || numberSecond === '') {
     alert('error');
-}
-else if(+numberSecond === 0) {
-        alert('error');
+}    
+else if(comparison1) {
+     alert('Are you sure you want to continue');
+     alert(' AdditionResult ' + additionResult);
+     alert(' Subtraction ' + subtractionResult);
+     alert(' Multiplication ' + multiplicationResult);
+     alert(' Division ' + divisionResult);
     }
-    else if(comparison) {
-            alert('Are you sure you want to continue');
-            alert(' Division ' + divisionResult);
+    else if(+numberSecond === 0) {
+        alert(' AdditionResult ' + additionResult);
+        alert(' Subtraction ' + subtractionResult);
+        alert(' Multiplication ' + multiplicationResult);
+        alert(' Division ' + ' error');
         }
-        else  alert(' Division ' + divisionResult);
+        else if(comparison2 && +numberFirst > 0 && +numberSecond > 0) {
+            alert(' AdditionResult ' + additionResult);
+            alert(' Subtraction ' + subtractionResult);
+            alert(' Multiplication ' + multiplicationResult);
+            alert(' Division ' + divisionResult);
+            }
+   
+
+    
+   
+
+       
+   
+
+
+       
+   
+ 
+
+       
+            
+
+ 
+
 
