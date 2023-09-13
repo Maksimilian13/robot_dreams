b9fab7c8c2b841a5b40109a4355f6a05cc64ef4c
Maksimilian13 <loskutov.maksxim@gmail.com>
2023-09-13 17:47:51 +0200

    lesson351

--- lesson35/main.js
+++ lesson35/main.js
@@ -1,20 +1,16 @@
 const userList = document.getElementById('user-list');
 
-// Функція для отримання списку користувачів з API
 async function fetchUsers() {
     try {
         const response = await fetch('https://gorest.co.in/public/v1/users');
         const data = await response.json();
 
-        // Очистити список користувачів перед оновленням
         userList.innerHTML = '';
 
-        // Додати користувачів до списку та додати обробник події для кожного користувача
         data.data.forEach(user => {
             const listItem = document.createElement('li');
             listItem.textContent = user.name;
             listItem.addEventListener('click', () => {
-                // Перейти на сторінку з постами з параметром userId
                 window.location.href = `user-posts.html?userId=${user.id}`;
             });
             userList.appendChild(listItem);
@@ -24,5 +20,4 @@ async function fetchUsers() {
     }
 }
 
-// Викликати функцію для отримання списку користувачів при завантаженні сторінки
 fetchUsers();
