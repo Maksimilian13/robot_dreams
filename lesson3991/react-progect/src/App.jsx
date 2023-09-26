439bf7cb0a3c56f3372020673acb72c1703118c4
Maksimilian13 <loskutov.maksxim@gmail.com>
2023-09-26 18:56:24 +0200

    lesson39

---
+++ lesson3991/react-progect/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Products from './components/Products';
+
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    const existingItem = cartItems.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      const updatedCart = cartItems.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      setCartItems(updatedCart);
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <Header cartItems={cartItems} />
+      <Products addToCart={addToCart} />
+    </div>
+  );
+}
+
+export default App;
